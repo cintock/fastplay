2a2e697e258bed0c56871c7a783b730a97e41072
Низамов Вадим <nizamovvr@bnipi.rosneft.ru>
2022-08-13 16:12:09 +0500

    Улучил алгоритм, чтобы пропускать битые кадры.

--- main.py
+++ main.py
@@ -13,35 +13,53 @@ def append_file(out: cv2.VideoWriter, input: cv2.VideoCapture):
     assert isinstance(input, cv2.VideoCapture)
     key = -1
     eof = False
+
+    # счетчик неудачных попыток подряд получить кадр
     empty_count = 0
+
+    # счетчик удачно полученных подряд кадров
+    good_frames = 0
+
     frame_times = []
     while input.isOpened() and key != 27 and not eof:
         start_t = time.time()
         # vc.set(cv2.CAP_PROP_POS_FRAMES, frame_no)
-        frame_no = input.get(cv2.CAP_PROP_POS_FRAMES)
 
-        ret, frame = input.read()
+        ret = input.grab()
         if not ret:
+            good_frames = 0
             empty_count += 1
+
             # больше 150 подряд кадров не читаются (критерий, что файл завершен)
             # если только несколько кадров не читается, то это может быть просто битый участок
             eof = empty_count >= 150
         else:
+            good_frames += 1
             empty_count = 0
 
-        if ret:
-            frame = cv2.resize(frame, (1920, 1080))
-            cv2.imshow('frame', frame)
+        # чтобы кадр стал "хороший" (картинка стабилизировалась после ключевого кадра),
+        # нужно после начала того, как что-то получено получить еще N кадров подряд
+        if good_frames >= 100:
+            ret, frame = input.retrieve()
 
-            out.write(frame)
+            if ret:
+                frame = cv2.resize(frame, (1920, 1080))
+                cv2.imshow('frame', frame)
 
-            for i in range(120):
-                input.grab()
+                out.write(frame)
 
-            key = cv2.waitKey(1)
-            end_t = time.time()
-            frame_time = end_t - start_t
-            frame_times.append(frame_time)
+                # пропускаем кадры (решение CAP_PROP_POS_FRAMES не срабатывает как надо для данного типа видео)
+                for i in range(110):
+                    input.grab()
+
+                key = cv2.waitKey(1)
+                end_t = time.time()
+                frame_time = end_t - start_t
+                frame_times.append(frame_time)
+            else:
+                good_frames = 0
+                print('Can not retrieve grabbed frame!')
+
     avg_frame_time = numpy.average(frame_times)
     print('avg frame time: {0}'.format(avg_frame_time))
 
@@ -74,4 +92,4 @@ if __name__ == '__main__':
                 input.release()
     finally:
         out.release()
-    cv2.destroyAllWindows()
+        cv2.destroyAllWindows()
